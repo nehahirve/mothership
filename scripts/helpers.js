8740314393e0170f28b1e613ecdebf5a2bda7c03
nehahirve <neha.s.hirve@gmail.com>
2020-10-23 09:41:08 +0200

    starfield as a SASS mixin

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -38,7 +38,6 @@ function getRandom(array) {
   This function loads a starfield
 */
 
-
 function loadStars(element) {
   for (let i = 0; i < 2; i++) {
     const star = document.createElement('div')
@@ -46,6 +45,8 @@ function loadStars(element) {
     star.style.position = 'absolute'
     star.style.top = '0'
     element.appendChild(star)
-
   }
 }
+
+const main = document.querySelector('main')
+loadStars(main)
